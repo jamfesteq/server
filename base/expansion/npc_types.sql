9d6be4a8297e99c0920a55897efbe5b70c0f9dc7
RebuildEQ <RebuildEQ@gmail.com>
2024-03-31 12:15:22 +0000

    Nerf aggro/assist radius

---
+++ base/expansion/npc_types.sql
@@ -0,0 +1,1 @@
+UPDATE npc_types SET aggroradius = aggroradius / 10, assistradius = assistradius / 10 WHERE aggroradius > 0 and assistradius > 0;UPDATE npc_types SET aggroradius = aggroradius / 10, assistradius = assistradius / 10 WHERE aggroradius > 0 and assistradius > 0;
