1a5583b50c167ae19f0fc6939722a94d66dc0d9d
RebuildEQ <RebuildEQ@gmail.com>
2024-04-01 12:28:05 +0000

    Fix DODH rules

--- base/expansion/10-dodh/rules.sql
+++ base/expansion/10-dodh/rules.sql
@@ -1,4 +1,4 @@
 # Darkhollow Client-Based Expansion Setting
 UPDATE rule_values SET rule_value='10' WHERE rule_name='Expansion:CurrentExpansion';
-UPDATE rule_values SET rule_value='512' WHERE rule_name='World:CharacterSelectExpansionSettings';
-UPDATE rule_values SET rule_value='512' WHERE rule_name='World:ExpansionSettings';
+UPDATE rule_values SET rule_value='1023' WHERE rule_name='World:CharacterSelectExpansionSettings';
+UPDATE rule_values SET rule_value='1023' WHERE rule_name='World:ExpansionSettings';
