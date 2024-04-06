2b87cc515bbead69c1d78c7eb96c4d415abaaf54
xackery <xackery@gmail.com>
2024-04-06 13:29:10 +0000

    Enable defiant drops

--- base/expansion/npc_types.sql
+++ base/expansion/npc_types.sql
@@ -3,3 +3,5 @@ UPDATE npc_types SET aggroradius = 25, assistradius = 0 where aggroradius > 0;
 UPDATE spawn2 SET min_expansion = 0 WHERE spawngroupid = 148650;
 # Iason from 15 to 0
 UPDATE spawn2 SET min_expansion = 0 WHERE spawngroupid = 110163;
+
+UPDATE global_loot SET min_expansion = 0 WHERE id IN (1,2,3,4,5,6,7,8);
