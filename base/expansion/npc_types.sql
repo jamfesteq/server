03410c858b12d7ccef58674e347b041167544787
xackery <xackery@gmail.com>
2024-04-06 13:14:48 +0000

    Add defiant

--- base/expansion/npc_types.sql
+++ base/expansion/npc_types.sql
@@ -1,1 +1,4 @@
 UPDATE npc_types SET aggroradius = 25, assistradius = 0 where aggroradius > 0;
+
+UPDATE spawn2 SET min_expansion = 0 WHERE id = 148650; # Keefe from 15 to 0
+UPDATE spawn2 SET min_expansion = 0 WHERE id = 110163; # Iason from 15 to 0
