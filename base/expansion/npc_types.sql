0833edf782ac50821124a3c6dffdfa42b11d0e30
xackery <xackery@gmail.com>
2024-04-06 13:21:13 +0000

    Fix where clause

--- base/expansion/npc_types.sql
+++ base/expansion/npc_types.sql
@@ -1,5 +1,5 @@
 UPDATE npc_types SET aggroradius = 25, assistradius = 0 where aggroradius > 0;
 # Keefe from 15 to 0
-UPDATE spawn2 SET min_expansion = 0 WHERE id = 148650;
+UPDATE spawn2 SET min_expansion = 0 WHERE spawngroupid = 148650;
 # Iason from 15 to 0
-UPDATE spawn2 SET min_expansion = 0 WHERE id = 110163;
+UPDATE spawn2 SET min_expansion = 0 WHERE spawngroupid = 110163;
