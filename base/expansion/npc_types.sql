de4b900fa3322e48bbdfd8f425ceb53626d5279e
xackery <xackery@gmail.com>
2024-04-06 13:20:14 +0000

    adjust comments

--- base/expansion/npc_types.sql
+++ base/expansion/npc_types.sql
@@ -1,4 +1,5 @@
 UPDATE npc_types SET aggroradius = 25, assistradius = 0 where aggroradius > 0;
-
-UPDATE spawn2 SET min_expansion = 0 WHERE id = 148650; # Keefe from 15 to 0
-UPDATE spawn2 SET min_expansion = 0 WHERE id = 110163; # Iason from 15 to 0
+# Keefe from 15 to 0
+UPDATE spawn2 SET min_expansion = 0 WHERE id = 148650;
+# Iason from 15 to 0
+UPDATE spawn2 SET min_expansion = 0 WHERE id = 110163;
