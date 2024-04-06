4c9b04ca6ce4916f0b448f5f2e3d2bc0aa045970
xackery <xackery@gmail.com>
2024-04-06 12:08:54 +0000

    Introduce OoW

--- base/expansion/rules.sql
+++ base/expansion/rules.sql
@@ -5,8 +5,8 @@ source 3-luclin/rules.sql;
 source 4-pop/rules.sql;
 source 5-loy/rules.sql;
 source 6-ldon/rules.sql;
-#source 7-god/rules.sql;
-#source 8-oow/rules.sql;
+source 7-god/rules.sql;
+source 8-oow/rules.sql;
 #source 9-don/rules.sql;
 #source 10-dodh/rules.sql;
 #source 11-por/rules.sql;
@@ -21,7 +21,7 @@ source 6-ldon/rules.sql;
 
 # Jamfest Overrides
 
-UPDATE rule_values SET rule_value='This is an early alpha of jamfest. Currently on Lost Dungeons of Norrath, #opt in to cool commands to speed things up. Check out https://jamfesteq.com for patcher and intro. Expect characters to be deleted for beta. Guildmasters are handy, hail them.' WHERE rule_name='World:MOTD';
+UPDATE rule_values SET rule_value='This is an early alpha of jamfest. Currently on Omens of War, #opt in to cool commands to speed things up. Check out https://jamfesteq.com for patcher and intro. Expect characters to be deleted for beta. Guildmasters are handy, hail them.' WHERE rule_name='World:MOTD';
 
 # Gear left on Corpses
 UPDATE rule_values SET rule_value='false' WHERE rule_name='Character:LeaveCorpses';
