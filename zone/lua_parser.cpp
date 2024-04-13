ce0f0ee208978c91d3a4212a087ccae901dc3e87
xackery <xackery@gmail.com>
2024-04-13 00:57:17 +0000

    Fix exp_source

--- zone/lua_parser.cpp
+++ zone/lua_parser.cpp
@@ -1310,6 +1310,7 @@ void LuaParser::MapFunctions(lua_State *L) {
 			lua_register_ruleb(),
 			lua_register_journal_speakmode(),
 			lua_register_journal_mode(),
+			lua_register_exp_source(),
 			lua_register_expedition(),
 			lua_register_expedition_lock_messages(),
 			lua_register_buff()
